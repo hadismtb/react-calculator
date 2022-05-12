50a7af0361234bb5096fd1f0e12f900987ad311f
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-12 17:20:43 +0430

    create add digit feature for calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 // STYLES
 import styles from "./Calculator.module.css";
 
+const initialState = {
+    previosOperand: "",
+    currentOperand: "",
+    operation: ""
+}
+
+const stateReducer = (state, action) => {
+    const {type, payload} = action;
+
+    switch(type) {
+        case "ADD_DIGIT":
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand || ""}${payload.digit}`
+            }
+    }
+}
+
 const Calculator = () => {
+    const [state, dispatch] = useReducer(stateReducer, initialState);
+
+    const {previosOperand, currentOperand} = state;
+
     return (
         <div className={styles.container}>
             <div className={styles.screen}>
-                <div className={styles.previous_operand}>12354 *</div>
-                <div className={styles.current_operand}>687</div>
+                <div className={styles.previous_operand}>{previosOperand}</div>
+                <div className={styles.current_operand}>{currentOperand}</div>
             </div>
             <button className={styles.two_columns}>AC</button>
             <button>DEL</button>
             <button>/</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "1"}})}>1</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "2"}})}>2</button>
+            <button  onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "3"}})}>3</button>
             <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "4"}})}>4</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "5"}})}>5</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "6"}})}>6</button>
             <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "7"}})}>7</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "8"}})}>8</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "9"}})}>9</button>
             <button>-</button>
-            <button>.</button>
-            <button>0</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "."}})}>.</button>
+            <button onClick={() => dispatch({type: "ADD_DIGIT", payload: {digit: "0"}})}>0</button>
             <button className={styles.two_columns}>=</button>
         </div>
     );
